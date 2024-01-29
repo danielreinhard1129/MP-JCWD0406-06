f2e0996af86a932260c98577100d54593e95793f
baso_afriza <134659802+Baso-CODE@users.noreply.github.com>
2024-01-29 22:59:30 +0700

    fix: bug (#10)

--- apps/web/src/app/register/components/CardRegister.tsx
+++ apps/web/src/app/register/components/CardRegister.tsx
@@ -2,23 +2,20 @@
 
 "use client";
 
-
-
-import axios, { AxiosError } from 'axios';
-import { Button } from 'flowbite-react';
-import Link from 'next/link';
-import { useRouter } from 'next/navigation';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import * as yup from 'yup';
-import YupPassword from 'yup-password';
-import { IForm } from '../../../../types/form.type';
-import { RegisterForm } from './RegisterForm';
-import { baseUrl } from '@/utils/config';
+import axios, { AxiosError } from "axios";
+import { Button } from "flowbite-react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import * as yup from "yup";
+import YupPassword from "yup-password";
+import { IForm } from "../../../../types/form.type";
+import { RegisterForm } from "./RegisterForm";
 YupPassword(yup);
 
 const RegisterCard = () => {
-
+  const baseUrl = "http://localhost:8000/api";
   const router = useRouter();
 
   const handleSubmit = async (values: IForm) => {
@@ -46,10 +43,8 @@ const RegisterCard = () => {
         theme: "light",
       });
       setTimeout(() => {
-
-        router.push('/login');
-      }, 1000);
-
+        router.push("/login");
+      }, 3000);
     } catch (error) {
       if (error instanceof AxiosError) {
         const errorMsg = error.response?.data || error.message;
@@ -72,9 +67,7 @@ const RegisterCard = () => {
             <h1 className="text-zinc-300 font-bold text-2xl ml-3 mb-5 mt-5">
               Welcome Back!
             </h1>
-
-
-
+            <p className="text-center  text-yellow-200 text-sm mx-auto w-full md:w-auto">
               Already have an account ?
               <br /> To keep connected with us please login with your personal
               info
@@ -87,9 +80,8 @@ const RegisterCard = () => {
           </div>
           <div className="w-full md:w-1/2 p-5">
             <div className="flex justify-center bg-white h-full rounded-md">
-
-              <RegisterForm onSubmit={handleSubmit} />
-
+              <RegisterForm onSubmit={handleSubmit} />{" "}
+              {/* use RegisterForm here */}
             </div>
           </div>
         </div>
--- apps/web/src/app/register/page.tsx
+++ apps/web/src/app/register/page.tsx
@@ -2,7 +2,7 @@ import CardRegister from "./components/CardRegister";
 import NextTopLoader from "nextjs-toploader";
 
 const Register = () => {
-  return 
+  return (
     <main className="container max-w-7xl mx-auto w-[100%]">
       <NextTopLoader color="#ffffff" showSpinner={false} />
       <CardRegister />
