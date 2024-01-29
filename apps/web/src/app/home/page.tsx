d8897c956b4075623fcaa6a06490e3315a4a5eee
willys123 <136543857+willys123@users.noreply.github.com>
2024-01-29 23:43:46 +0700

    Sym 004/reff and points (#11)
    * refactor: update prisma table
    
    * feat: refferall and points

--- apps/web/src/app/home/page.tsx
+++ apps/web/src/app/home/page.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import HeroHome from "./components/HeroHome";
-import FeatureProduct from "@/components/FeatureProduct";
-import TopProduct from "@/components/TopProduct";
-import NewsEvent from "@/components/NewsEvent";
-import Contact from "@/components/Contact";
-import SliderBanner from "./components/SliderBanner";
+import React from 'react';
+import HeroHome from './components/HeroHome';
+import FeatureProduct from '@/components/FeatureProduct';
+import TopProduct from '@/components/TopProduct';
+import NewsEvent from '@/components/NewsEvent';
+import Contact from '@/components/Contact';
+import SliderBanner from './components/SliderBanner';
 
 const Homepage = () => {
   return (
--- apps/web/src/app/register/components/RegisterForm.tsx
+++ apps/web/src/app/register/components/RegisterForm.tsx
@@ -1,13 +1,14 @@
-
+import { baseUrl } from '@/utils/config';
+import axios, { AxiosError } from 'axios';
 import { Button, Label, Select, TextInput } from 'flowbite-react';
 import { useFormik } from 'formik';
-import { IForm } from '../../../../types/form.type';
-import { validationSchema } from './validationSchema';
-import PhoneInput from 'react-phone-number-input';
-import 'react-phone-number-input/style.css';
 import { useState } from 'react';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
-
+import PhoneInput from 'react-phone-number-input';
+import 'react-phone-number-input/style.css';
+import { toast } from 'sonner';
+import { IForm } from '../../../../types/form.type';
+import { validationSchema } from './validationSchema';
 
 interface RegisterFormProps {
   onSubmit: (values: IForm) => void;
@@ -16,21 +17,42 @@ interface RegisterFormProps {
 export const RegisterForm = ({ onSubmit }: RegisterFormProps) => {
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [isReferralValid, setIsReferralValid] = useState(false);
+  const [referralError, setReferralError] = useState<string | null>(null);
+  const [referralCode, setReferralCode] = useState('');
 
   const formik = useFormik({
     initialValues: {
-      fullName: "",
-      password: "",
-      email: "",
-      contact: "",
-      address: "",
-      referral_number: "",
-      roleId: "",
-      confirmPassword: "",
+      fullName: '',
+      password: '',
+      email: '',
+      contact: '',
+      address: '',
+      referral_number: '',
+      roleId: '',
+      confirmPassword: '',
     },
     validationSchema,
-    onSubmit,
+    onSubmit: (values) => {
+      onSubmit(values);
+    },
   });
+
+  const handleClaimClick = async () => {
+    try {
+      await axios.post(baseUrl + '/users/claim-refferal', {
+        referralCode: formik.values.referral_number,
+      });
+      toast.success('Success claim referral');
+      setIsReferralValid(true); // set isReferralValid to true here
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        const errorMsg = error.response?.data || error.message;
+        toast.error(errorMsg);
+        console.error(error);
+      }
+    }
+  };
 
   return (
     <form
@@ -57,10 +79,8 @@ export const RegisterForm = ({ onSubmit }: RegisterFormProps) => {
           <p className="text-red-500 text-sm mt-1">{formik.errors.fullName}</p>
         )}
       </div>
-     
-
       <div className="md:flex gap-2 flex-col md:flex-row">
-
+        <div className="w-full md:w-1/2">
           <label htmlFor="email" className="mb-2 block">
             Email
           </label>
@@ -71,14 +91,12 @@ export const RegisterForm = ({ onSubmit }: RegisterFormProps) => {
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
             value={formik.values.email}
-
+            className="border-white"
           />
           {formik.errors.email && formik.touched.email && (
             <p className="text-red-500 text-sm mt-1">{formik.errors.email}</p>
           )}
         </div>
-
-
         <div className="w-full md:w-1/2">
           <div className="mb-2 block">
             <Label htmlFor="contact" value="Contact" />
@@ -87,7 +105,7 @@ export const RegisterForm = ({ onSubmit }: RegisterFormProps) => {
             international
             defaultCountry="ID"
             value={formik.values.contact}
-            onChange={(value) => formik.setFieldValue("contact", value)}
+            onChange={(value) => formik.setFieldValue('contact', value)}
             onBlur={formik.handleBlur}
             id="contact"
             name="contact"
@@ -173,10 +191,10 @@ export const RegisterForm = ({ onSubmit }: RegisterFormProps) => {
           <p className="text-red-500 text-sm mt-1">{formik.errors.address}</p>
         )}
       </div>
-      <div>
-        <div className="mb-2 block">
-          <Label htmlFor="referral_number" value="Referral Number (Optional)" />
-        </div>
+      <div className="mb-2 block">
+        <Label htmlFor="referral_number" value="Referral Number (Optional)" />
+      </div>
+      <div className="flex gap-2">
         <TextInput
           value={formik.values.referral_number}
           onChange={formik.handleChange}
@@ -184,13 +202,21 @@ export const RegisterForm = ({ onSubmit }: RegisterFormProps) => {
           id="referral_number"
           type="text"
           shadow
+          className="w-full"
         />
-        {formik.errors.referral_number && formik.touched.referral_number && (
-          <p className="text-red-500 text-sm mt-1">
-            {formik.errors.referral_number}
-          </p>
-        )}
+        <button
+          type="button"
+          onClick={handleClaimClick}
+          className="p-2 bg-black rounded-md text-nowrap text-white hover:cursor-pointer hover:bg-slate-600"
+        >
+          Claim
+        </button>
       </div>
+      {formik.errors.referral_number && formik.touched.referral_number && (
+        <p className="text-red-500 text-sm mt-1">
+          {formik.errors.referral_number}
+        </p>
+      )}
       <div className="block">
         <Label htmlFor="roleId" value="Role" />
       </div>
@@ -201,8 +227,8 @@ export const RegisterForm = ({ onSubmit }: RegisterFormProps) => {
         onChange={formik.handleChange}
       >
         <option value="">Select your Role</option>
-        <option value={"customer"}>Customer</option>
-        <option value={"promotor"}>Promotor</option>
+        <option value={'customer'}>Customer</option>
+        <option value={'promotor'}>Promotor</option>
       </Select>
       <Button color="dark" type="submit">
         Submit
